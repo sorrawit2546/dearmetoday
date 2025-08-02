57513d8be37d7d769b9530c289e381c26d18849b
inthegarden <sorrawit.intern@myorder.ai>
2025-08-03 00:49:13 +0700

    update

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,16 +1,23 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
 
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), 
+    provideRouter(routes),
     provideClientHydration(withEventReplay()),
-    provideHttpClient()
-  ]
+    provideHttpClient(withFetch()),
+  ],
 };
