89ad551cee3ead78f2c11647748ddcc9a9e812b0
inthegarden <sorrawit.intern@myorder.ai>
2025-09-08 16:49:35 +0700

    Update auth.service.ts

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -16,14 +16,12 @@ export class AuthService {
     // 1. ตรวจว่าผู้ใช้นี้เคย login หรือยัง
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     let user = await this.authRepo.findUserByGoogleId(googleUser.id);
-
     // 2. ถ้าไม่เคย ก็สร้างใหม่
     if (!user) {
       user = (await this.authRepo.createWithGoogle(
         googleUser,
       )) as unknown as User;
     }
-
     // 3. สร้าง JWT
     const accessToken = this.jwtService.sign({
       sub: (user as User).id,
