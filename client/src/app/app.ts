459185bf994400915d3ba29e078848e5a00e1b46
9Sorrawit <83335817+sorrawit2546@users.noreply.github.com>
2025-10-15 12:37:50 +0700

    Update app.ts

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { Router, NavigationEnd, RouterOutlet } from '@angular/router';
+import { filter } from 'rxjs';
+
+declare let gtag: Function; // ← ใช้ฟังก์ชันจาก gtag.js ที่โหลดใน index.html
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'Dearme,Today';
+  private router = inject(Router);
+
+  ngOnInit() {
+    // ✅ ติดตามทุกครั้งที่เปลี่ยน route
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        const pagePath = event.urlAfterRedirects;
+
+        // ส่งข้อมูล page_view ไป GA4
+        gtag('event', 'page_view', {
+          page_path: pagePath,
+          page_title: document.title,
+        });
+
+        // (ถ้าอยาก log ใน console ด้วย)
+        console.log('📊 GA4 page_view tracked:', pagePath);
+      });
+  }
 }
