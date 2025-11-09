d6dc7d9dcdde0b5d5642c0aefaf33f246a58a43e
dvaxwd <dvaxwd12090@gmail.com>
2025-11-09 17:05:56 +0700

    แก้โดน lint

--- server/src/seed-stack/seed-stack.controller.ts
+++ server/src/seed-stack/seed-stack.controller.ts
@@ -1,29 +1,37 @@
-import { Controller, Get, Post, Put, Delete, Req, Res, HttpCode} from "@nestjs/common";
-import type { Request, Response} from "express";
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Req,
+  HttpCode,
+} from '@nestjs/common';
+import type { Request, Response } from 'express';
 
 @Controller('seeds')
 export class SeedsController {
-    // This function is used to fetch data in stack table
-    @Get("stacks")
-    GetStack(@Req() req: Request): string{
-        console.log(req)
-        return "req"
-    }
+  // This function is used to fetch data in stack table
+  @Get('stacks')
+  GetStack(@Req() req: Request): string {
+    console.log(req);
+    return 'req';
+  }
 
-    // This function is used to insert data in stack table
-    @Post("stacks")
-    @HttpCode(201)
-    SetStack(): string{
-        return "Created"
-    }
+  // This function is used to insert data in stack table
+  @Post('stacks')
+  @HttpCode(201)
+  SetStack(): string {
+    return 'Created';
+  }
 
-    @Put("stacks")
-    Update(): String{
-        return
-    }
+  @Put('stacks')
+  Update(): string {
+    return;
+  }
 
-    @Delete("stacks")
-    DeleteStack(): string{
-        return
-    }
+  @Delete('stacks')
+  DeleteStack(): string {
+    return;
+  }
 }
